4c10eba50e88eaa2228c08c9c89d0289036a8135
Shravika Mittal <shravika16093@iiitd.ac.in>
2017-07-26 23:33:28 +0530

    Removed comments

--- pin-pull.js
+++ pin-pull.js
@@ -1,7 +1,3 @@
-//can you see this?
-//need to see how to write the pull ka code from other examples
-//should the callback funtion be also included in the code? Yes -- see write ka example -- but they havent wriiten write method as such, I think we should remove pull() which I just added
-
 var tessel= require('tessel');
 
 var pin = tessel.port.A.pin[2];
