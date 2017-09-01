9349a36440afbd3cef5c4b01f5f7df782299dabb
Brihi <brihi16142@iiitd.ac.in>
2017-09-01 22:47:37 +0530

    Added x,y,z readings

--- i2c.js
+++ i2c.js
@@ -13,7 +13,28 @@ i2c.read(numBytesToRead, function (error, dataReceived) {
   // Print data received (buffer of hex values)
   console.log('Buffer returned by I2C slave device ('+slaveAddress.toString(16)+'):', dataReceived);
 });
+
 i2c.transfer(new Buffer([0x0D]), numBytesToRead, function (error, dataReceived) {
     // Print data received (buffer of hex values)
   console.log('Buffer returned by I2C slave device ('+slaveAddress.toString(16)+'):', dataReceived);
+
+});
+i2c.transfer(new Buffer([0x01]), 6, function (error, dataReceived) {
+    // Print data received (buffer of hex values)
+    if (error) throw error;
+
+    var out=[];
+    for (var i=0;i<3;i++){
+      var gCount=(dataReceived[i*2] << 8) | dataReceived[(i*2)+1];
+      gCount=gCount >> 4;
+
+      if (dataReceived[i*2] > 0x7F) {
+          gCount = -(1 + 0xFFF - gCount); // Transform into negative 2's complement
+        }
+
+        out[i] = gCount / ((1<<12)/(2*2));
+    }
+
+    console.log('The x, y, z values are :',out);
+
 });
