7a11e09751bc0f3c65559b593dbc21de61304a19
Shravika Mittal <shravika16093@iiitd.ac.in>
2017-09-02 00:08:28 +0530

    Update i2c.js

--- i2c.js
+++ i2c.js
@@ -1,24 +1,24 @@
 var tessel = require('tessel');
 
 // Connect to device
-var port = tessel.port.A; // Use the SCL/SDA pins of Port A
-var slaveAddress = 0x1D; // Specific to device
+var port = tessel.port.A; // Select Port A of Tessel
+var slaveAddress = 0x1D; // Specefic for accelerometer module
 var i2c = new port.I2C(slaveAddress); // Initialize I2C communication
 
 // Details of I2C transfer
 var numBytesToRead = 1; // Read back this number of bytes
 
-// Read data over I2C using i2c.transfer
 i2c.read(numBytesToRead, function (error, dataReceived) {
   // Print data received (buffer of hex values)
   console.log('Buffer returned by I2C slave device ('+slaveAddress.toString(16)+'):', dataReceived);
 });
 
+// Read/Receive data over I2C using i2c.transfer
 i2c.transfer(new Buffer([0x0D]), numBytesToRead, function (error, dataReceived) {
     // Print data received (buffer of hex values)
   console.log('Buffer returned by I2C slave device ('+slaveAddress.toString(16)+'):', dataReceived);
-
 });
+
 i2c.transfer(new Buffer([0x01]), 6, function (error, dataReceived) {
     // Print data received (buffer of hex values)
     if (error) throw error;
@@ -36,5 +36,4 @@ i2c.transfer(new Buffer([0x01]), 6, function (error, dataReceived) {
     }
 
     console.log('The x, y, z values are :',out);
-
 });
